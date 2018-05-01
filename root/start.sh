b073790de6c422eaa54799d57de715ae57965e27
Poag <Poag@users.noreply.github.com>
2018-05-01 13:33:51 +0100

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script to download tv shows from BBC iPlayer
-/usr/local/bin/get_iplayer_web_pvr --listen 127.0.0.1 --port 1935
+/usr/bin/get_iplayer_web_pvr --listen 127.0.0.1 --port 1935
