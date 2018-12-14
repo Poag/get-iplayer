a057b311baae688e51bb941b6ac86b32667fb857
Poag <Poag@users.noreply.github.com>
2018-12-14 12:59:08 +0000

    Update start.sh

--- root/start.sh
+++ root/start.sh
@@ -38,7 +38,7 @@ do
 
 		echo "[info] Running get_iplayer..."
 		# run get_iplayer for show, saving to incomplete folder
-		/usr/bin/get_iplayer --profile-dir /config --get --nopurge --modes=hlshd,hlsvhigh,hlsstd,hlsstd,hlsnormal,hlsxstd --file-prefix="${show_name} - <senum> - <episodeshort>" "${show_name}" --output "/data/get_iplayer/incomplete/${show_name}"
+		/usr/bin/get_iplayer --profile-dir /config --get --nopurge --modes=hlshd,hvfxsd,hvfxhigh,hvfxhigh,hlsnormal,hvfxhigh --file-prefix="${show_name} - <senum> - <episodeshort>" "${show_name}" --output "/data/get_iplayer/incomplete/${show_name}"
 
 	done
 
